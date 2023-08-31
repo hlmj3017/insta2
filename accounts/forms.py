4dcaaa5683ba95dfdeadd9482f84724a262b9e2b
hlmj3 <hlmj3017@naver.com>
2023-08-31 17:27:05 +0900

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
 from django.contrib.auth import get_user_model
 
 class UserForm(UserCreationForm):
     class Meta:
         model = get_user_model()
 
-        fields = ('username', 'profile_image')
+        fields = ('username', 'profile_image',)
 
 class AuthForm(AuthenticationForm):
     pass
--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
     posts = Post.objects.all().order_by('-id')
+    comment_form = CommentForm()
 
     context= {
         'posts' : posts,
+        'comment_form': comment_form,
     }
 
     return render(request,'index.html',context)
 
-
+@login_required
 def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
@@ -30,3 +33,36 @@ def create(request):
     }
 
     return render(request, 'form.html', context)
+
+
+@login_required
+def comment_create(request, post_id):
+    comment_form = CommentForm(request.POST) # 인스턴스화
+
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        # 현재 로그인 유저
+        comment.user = request.user
+        # post_id를 기준으로 찾은 post
+        post = Post.objects.get(id=post_id)
+        comment.post = post
+
+        # comment.post_id = post_id : 빠르게 하고 싶은 경우
+
+        comment.save()
+
+        return redirect('posts:index')
+
+@login_required
+def like(request, post_id):
+    user = request.user
+    post = Post.objects.get(id=post_id)
+
+    if post in user.like_posts.all():
+        pass
+        # post.like_users.remove(user)
+
+    else:
+        post.like_users.add(user)
+
+    return redirect('posts:index')
